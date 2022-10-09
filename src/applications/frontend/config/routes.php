ee4ee665aab9f643bede2e2ebfd27b93b5c0d9c5
Fedor B Gorsky <fedor@support-pc.org>
2022-10-09 09:39:51 +0500

    tune default config

--- src/applications/frontend/config/routes.php
+++ src/applications/frontend/config/routes.php
@@ -3,6 +3,7 @@
 declare(strict_types = 1);
 
 return [
+    '/' => 'site/index',
     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
 ];
